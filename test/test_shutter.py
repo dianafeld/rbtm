49498fae0599dbe99e45d0ef0ad7b590a9abc94b
Рябова Мария Владимировна <mariya.ryabova@phystech.edu>
2015-03-21 23:09:07 +0100

    RBTM-122

--- test/test_shutter.py
+++ test/test_shutter.py
@@ -1,26 +1,26 @@
 import pytest
 import os, sys
 import PyTango
+import subprocess
 
 @pytest.yield_fixture
-def shutter():
-    cmdRunServer = 'python ../tango_ds/XRayShutter/XRayShutter.py shutter &'
-    cmdStopServer = 'killall shutter'
-    os.system(cmdRunServer)
+def shutter(request):
+    #cmdRunServer = 'python ../tango-ds/XRayShutter/XRayShutter.py shutter &'
+    #cmdStopServer = '''ps axf | grep shutter | grep -v grep | awk '{print "kill -9 " $1}' | sh'''
+    #os.system(cmdRunServer)
+    process = subprocess.Popen(['python', './tango-ds/XRayShutter/XRayShutter.py', 'shutter'])
     yield PyTango.DeviceProxy('tomo/shutter/1')
-    os.system(cmdStopServer)
+    request.addfinalizer(process.kill) 
+    #os.system(cmdStopServer)
 
 def test_shutter_init_state(shutter):
     assert shutter.State() == PyTango._PyTango.DevState.CLOSE
 
-def test_shutter_status(shutter):
-    assert shutter.Status() == 'The device is in CLOSE state.'
-
 def test_shutter_open(shutter):
     shutter.Open()
-    assert shutter.Status() == 'The device is in OPEN state.'
+    assert shutter.State() == PyTango._PyTango.DevState.OPEN
 
 def test_shutter_close(shutter):
     shutter.Open()
     shutter.Close()
-    assert shutter.Status() == 'The device is in CLOSE state.'
+    assert shutter.State() == PyTango._PyTango.DevState.CLOSE
--- test/test_source.py
+++ test/test_source.py
@@ -1,14 +1,69 @@
 import pytest
 import os, sys
 import PyTango
+import subprocess
+
+MAX_VOLTAGE = 100.0
+MIN_VOLTAGE = 10.0
+CORRECT_VOLTAGE = (MIN_VOLTAGE + MAX_VOLTAGE)/2
 
 @pytest.yield_fixture
-def source():
-    cmdRunServer = 'python ..tango_ds/XRaySource/XRaySource.py source &'
-    cmdStopServer = 'killall source'
-    os.system(cmdRunServer)
+def source(request):
+    #cmdRunServer = 'python ../tango-ds/XRaySource/XRaySource.py source &'
+    #cmdStopServer = '''ps axf | grep source | grep -v grep | awk '{print "kill -9 " $1}' | sh'''
+    #os.system(cmdRunServer)
+    process = subprocess.Popen(['python', '../tango-ds/XRaySource/XRaySource.py', 'source'])
     yield PyTango.DeviceProxy('tomo/source/1')
-    os.system(cmdStopServer)
+    request.addfinalizer(process.kill)    
+    #os.system(cmdStopServer)
 
-def test_shutter_init_state(source):
+#States testing
+def test_source_init_state(source):
     assert source.State() == PyTango._PyTango.DevState.OFF
+
+def test_source_on(source):
+    source.On()
+    assert source.State() == PyTango._PyTango.DevState.ON
+
+def test_source_off(source):
+    source.On()
+    source.Off()
+    assert source.State() == PyTango._PyTango.DevState.OFF
+"""
+#Voltage testing
+def test_source_init_voltage(source):
+    source.On()
+    assert source.voltage == MIN_VOLTAGE
+
+def test_source_set_correct_voltage(source):
+    source.On()
+    source.SetOperatingMode(CORRECT_VOLTAGE)
+    assert source.voltage == CORRECT_VOLTAGE
+
+def test_source_set_min_voltage(source):
+    source.On()
+    source.SetOperatingMode(MIN_VOLTAGE)
+    assert source.voltage == MIN_VOLTAGE
+
+def test_source_set_less_than_min_voltage(source):
+    source.On()
+    prevVoltage = source.voltage
+    source.SetOperatingMode(MIN_VOLTAGE - 0.1)
+    assert source.voltage == prevVoltage
+
+def test_source_set_max_voltage(source):
+    source.On()
+    source.SetOperatingMode(MAX_VOLTAGE)
+    assert source.voltage == MAX_VOLTAGE
+
+def test_source_set_more_than_max_voltage(source):
+    source.On()
+    prevVoltage = source.voltage
+    source.SetOperatingMode(MAX_VOLTAGE + 0.1)
+    assert source.voltage == prevVoltage
+
+#Testing voltage change in OFF state
+def test_source_set_voltage_if_off(source):
+    source.SetOperatingMode(CORRECT_VOLTAGE)
+"""
+    
