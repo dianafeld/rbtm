2dcf2afcc5a48ffb31af0a9d296a57517fad8804
Arlenka <alenarykunova@gmail.com>
2015-04-14 20:59:09 +0300

    RBTM-189 добавлена проверка корректности ответов модуля, исправлен баг, замеченный при тестировании (два запроса на считывание подряд)

--- tango_ds/XRayShutter/driver_shutter.py
+++ tango_ds/XRayShutter/driver_shutter.py
@@ -2,7 +2,7 @@
 __author__ = 'arlen'
 import serial
 import sys
-
+import re
 
 class Shutter:
     TIMEOUT = 1
@@ -18,7 +18,7 @@ class Shutter:
         # проверить работоспособность модуля
         self.port.write('$KE\r\n')
         if self.port.readline() != '#OK\r\n':
-            sys.stderr.write("Module isn't in workable state")
+            sys.stderr.write("Module isn't in workable state\n")
 
     def __init__(self):
         self.port = serial.Serial()
@@ -30,70 +30,83 @@ class Shutter:
             self.port.write("$KE,IO,SET," + str(i) + ",0,S\r\n")
             # проверить, что настройки применились верно
             if self.port.readline() != '#IO,SET,OK\r\n':
-                sys.stderr.write("Module can't set the line " + str(i) + " for output")
+                sys.stderr.write("Module can't set the line " + str(i) + " for output\n")
             # установить на линию i высокий уровень напряжения
             self.port.write("$KE,WR," + str(i) + ",1\r\n")
             # проверить, что установилось верно
-            if self.port.readline() == '#WR,WRONGLINE\r\n':
-                sys.stderr.write("The line " + str(i) + " is configured for input, module can't write to it")
-            elif self.port.readline() != '#WR,OK\r\n':
-                sys.stderr.write("Module can't access to the line " + str(i) + " (didn't set value)")
+            message = self.port.readline()
+            if message == '#WR,WRONGLINE\r\n':
+                sys.stderr.write("The line " + str(i) + " is configured for input, module can't write to it\n")
+            elif message != '#WR,OK\r\n':
+                sys.stderr.write("Module can't access to the line " + str(i) + " (didn't set value)\n")
 
         # настроить линию 4 в качестве входной
         self.port.write("$KE,IO,SET,4,1,S\r\n")
         if self.port.readline() != '#IO,SET,OK\r\n':
-            sys.stderr.write("Module can't set the line 4 for input")
+            sys.stderr.write("Module can't set the line 4 for input\n")
+
+    def answer_is_correct(self, message, regex):
+        pattern = re.compile(regex)
+        answer = pattern.match(message)
+        if answer != None:
+            if answer.group() == message:
+                return True
+        return False
 
     def is_open(self):
         # послать запрос на считывание состояния реле
         self.port.write("$KE,RDR,1\r\n")
 
-        # считать состояние реле
+        # считать состояние реле и проверить на корректность
         relay_state = self.port.readline().strip()
-        relay_state = relay_state.split(',')[2]
-        #TODO: проверка на корректность строки регулярным выражением (формат #RID,<RelayNumber>,<State>)
+        if self.answer_is_correct(relay_state, "#RID,(1|01),(1|0)"):
+            # взять значение из строчки
+            relay_state = relay_state.split(',')[2]
+        else:
+            sys.stderr.write("Unexpected answer from module, should be '#RID,1,<Value>', got '" + relay_state + "'\n")
 
         # послать запрос на считывание данных с 4 линии
         self.port.write("$KE,RD,4\r\n")
 
-        # считать данные с линии
+        # считать данные с линии и проверить на корректность
         data = self.port.readline().strip()
-        # проверка на корректность
         if data == "#RD,WRONGLINE":
-            sys.stderr.write("The line 4 is configured for output, module can't read from it")
-        #TODO: проверка на корректность строки регулярным выражением (формат #RD,<LineNumber>,<Value>)
-
-        # взять значение из строчки
-        data = data.split(',')[2]
+            sys.stderr.write("The line 4 is configured for output, module can't read from it\n")
+        if self.answer_is_correct(data, "#RD,(4|04),(1|0)"):
+            # взять значение из строчки
+            data = data.split(',')[2]
+        else:
+            sys.stderr.write("Unexpected answer from module, should be '#RD,4,<Value>', got '" + data + "'\n")
 
         # проверка на соответствие данных
         if data != relay_state:
             sys.stderr.write("The value of line 4 is not equal to relay_state: line value " + str(data) +
-                             ", expected: " + str(relay_state))
+                             ", expected: " + str(relay_state) + "\n")
         # лямбда-функция для переворачивания значения: 0->1, 1->0
         inverse = lambda x: (1, 0)[int(x)]
 
         # записать перевернутое значение на 1 линию
         self.port.write("$KE,WR,1," + str(inverse(data)) + "\r\n")
         # проверить, что записалось
-        if self.port.readline() == '#WR,WRONGLINE\r\n':
-            sys.stderr.write("The line 1 is configured for input, module can't write to it")
-        elif self.port.readline() != '#WR,OK\r\n':
-            sys.stderr.write("Module can't access to the line 1 (didn't set value)")
+        message = self.port.readline()
+        if message == '#WR,WRONGLINE\r\n':
+            sys.stderr.write("The line 1 is configured for input, module can't write to it\n")
+        elif message != '#WR,OK\r\n':
+            sys.stderr.write("Module can't access to the line 1 (didn't set value)\n")
 
         return [False, True][int(data)]
 
     def set_value(self, val):
         # 1 открыть заслону, 0 закрыть заслонку
         if not val in [0, 1]:
-            sys.stderr.write("Trying to set incorrect value to shutter: expected 0 or 1, got " + str(val))
+            sys.stderr.write("Trying to set incorrect value to shutter: expected 0 or 1, got " + str(val) + "\n")
 
         # установить значение на реле (1 замкнуть, 0 разомкнуть)
         self.port.write("$KE,REL,1," + str(val) + "\r\n")
 
         # проверить, что значение установилось
         if self.port.readline() != "#REL,OK\r\n":
-            sys.stderr.write("Can't set value " + str(val) + " to relay")
+            sys.stderr.write("Can't set value " + str(val) + " to relay\n")
 
     def open(self):
         self.set_value(1)
