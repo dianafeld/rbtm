d6dc9a6dae89707dac79f9adaa2013caef27bb7b
Arlenka <alenarykunova@gmail.com>
2015-04-14 14:13:14 +0300

    RBTM-187 added driver commands to PyTango Device, reorganised driver

--- tango_ds/XRayShutter/driver_shutter.py
+++ tango_ds/XRayShutter/driver_shutter.py
@@ -1,24 +1,27 @@
-# coding=utf-8
+# -*- coding: utf-8 -*-
 __author__ = 'arlen'
 import serial
 import sys
 
 
 class Shutter:
-    port = serial.Serial()
-    timeout = int()
+    TIMEOUT = 1
+
+    def close_port(self):
+        self.port.close()
 
     def open_port(self):
         # инициализировать порт
-        self.port = serial.Serial(4, timeout=self.timeout)
+        # '/dev/ttyACM0'
+        self.port = serial.Serial(4, timeout=Shutter.TIMEOUT)
 
         # проверить работоспособность модуля
         self.port.write('$KE\r\n')
         if self.port.readline() != '#OK\r\n':
             sys.stderr.write("Module isn't in workable state")
 
-    def __init__(self, timeout=1):
-        self.timeout = timeout
+    def __init__(self):
+        self.port = serial.Serial()
         # открыть порт
         self.open_port()
 
@@ -41,46 +44,47 @@ class Shutter:
         if self.port.readline() != '#IO,SET,OK\r\n':
             sys.stderr.write("Module can't set the line 4 for input")
 
-        # закрыть порт
-        self.port.close()
+    def is_open(self):
+        # послать запрос на считывание состояния реле
+        self.port.write("$KE,RDR,1\r\n")
 
-    def check_state(self):
-        # открыть порт
-        self.open_port()
-
-        # лямбда-функция для переворачивания значения: 0->1, 1->0
-        inverse = lambda x: (1, 0)[int(x)]
+        # считать состояние реле
+        relay_state = self.port.readline().strip()
+        relay_state = relay_state.split(',')[2]
+        #TODO: проверка на корректность строки регулярным выражением (формат #RID,<RelayNumber>,<State>)
 
         # послать запрос на считывание данных с 4 линии
         self.port.write("$KE,RD,4\r\n")
 
-        # считать данные
-        data = self.port.readline()
-
+        # считать данные с линии
+        data = self.port.readline().strip()
         # проверка на корректность
-        if data == "#RD,WRONGLINE\r\n":
+        if data == "#RD,WRONGLINE":
             sys.stderr.write("The line 4 is configured for output, module can't read from it")
         #TODO: проверка на корректность строки регулярным выражением (формат #RD,<LineNumber>,<Value>)
 
-        # взять значение из строчки, перевернуть на обратное
-        data = inverse(data.split(',')[2].replace('\r\n', ''))
+        # взять значение из строчки
+        data = data.split(',')[2]
+
+        # проверка на соответствие данных
+        if data != relay_state:
+            sys.stderr.write("The value of line 4 is not equal to relay_state: line value " + str(data) +
+                             ", expected: " + str(relay_state))
+        # лямбда-функция для переворачивания значения: 0->1, 1->0
+        inverse = lambda x: (1, 0)[int(x)]
 
         # записать перевернутое значение на 1 линию
-        self.port.write("$KE,WR,1," + str(data) + "\r\n")
+        self.port.write("$KE,WR,1," + str(inverse(data)) + "\r\n")
         # проверить, что записалось
         if self.port.readline() == '#WR,WRONGLINE\r\n':
             sys.stderr.write("The line 1 is configured for input, module can't write to it")
         elif self.port.readline() != '#WR,OK\r\n':
             sys.stderr.write("Module can't access to the line 1 (didn't set value)")
 
-        # закрыть порт
-        self.port.close()
-        return [True, False][int(data)]
+        return [False, True][int(data)]
 
     def set_value(self, val):
-        # открыть порт
-        self.open_port()
-
+        # 1 открыть заслону, 0 закрыть заслонку
         if not val in [0, 1]:
             sys.stderr.write("Trying to set incorrect value to shutter: expected 0 or 1, got " + str(val))
 
@@ -91,6 +95,8 @@ class Shutter:
         if self.port.readline() != "#REL,OK\r\n":
             sys.stderr.write("Can't set value " + str(val) + " to relay")
 
-        # закрыть порт
-        self.port.close()
+    def open(self):
+        self.set_value(1)
 
+    def close(self):
+        self.set_value(0)
