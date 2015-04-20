c7072ade8650187e110fef22087a5b0d671d3840
anna.savinova <anna.savinova@phystech.edu>
2015-04-20 18:20:45 +0300

    RBTM-155

--- robotom/storage/views.py
+++ robotom/storage/views.py
@@ -5,30 +5,33 @@ from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 def storage_view(request):
     # TODO
+    records_list = range(110)
+    pagin = Paginator(records_list, 15)
+    page = request.GET.get('page')
 
+    try:
+        records = pagin.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        records = pagin.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        records = pagin.page(pagin.num_pages)
+
+    to_show = True
     if request.method == "GET":
-        return render(request, 'storage/storage_index.html', {
-            'caption': 'Хранилище',
-            'toShowResult': False,
-        })
+        if page is None:
+            to_show = False
     elif request.method == "POST":
-        records_list = range(110)
-        pagin = Paginator(records_list, 20)
-        page = request.GET.get('page')
+        records = pagin.page(1)
 
-        try:
-            records = pagin.page(page)
-        except PageNotAnInteger:
-            # If page is not an integer, deliver first page.
-            records = pagin.page(1)
-        except EmptyPage:
-            # If page is out of range (e.g. 9999), deliver last page of results.
-            records = pagin.page(pagin.num_pages)
-        return render(request, 'storage/storage_index.html', {
-            'record_range': records,
-            'caption': 'Хранилище',
-            'toShowResult': True,
-        })
+    return render(request, 'storage/storage_index.html', {
+        'record_range': records,
+        'caption': 'Хранилище',
+        'toShowResult': to_show,
+        'pages': xrange(1, pagin.num_pages + 1),
+        'current_page': page,
+    })
 
 
 def storage_record_view(request, storage_record_id):
