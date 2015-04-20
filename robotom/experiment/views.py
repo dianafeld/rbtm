2d78503dabfba73a87719269b289ede83ea6d0cd
sobolev.1994 <sobolev.1994@mail.ru>
2015-04-20 21:48:55 +0400

    RBTM-163 RBTM-164

--- robotom/experiment/views.py
+++ robotom/experiment/views.py
@@ -6,11 +6,16 @@ from django.contrib.auth.decorators import login_required, user_passes_test
 def has_experiment_access(user):
     return user.userprofile.role in ['ADM', 'EXP']
 
+def power_on(request):
+	requests.get('http://109.234.34.140:5001/module-experiment/v1.0/source/power-on')
+	return render(request)
+
+
 
 @login_required
 @user_passes_test(has_experiment_access)
 def experiment_view(request):
-    return render(request, 'experiment/experiment_index.html', {
+    return render(request, 'experiment/admin_rus/adj_admin.html', {
         'full_access': (request.user.userprofile.role == 'EXP'),
         'caption': 'Эксперимент',
     })
