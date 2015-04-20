83f0eccd9a1c94935a5a4a6ededc788ba0f48941
anna.savinova <anna.savinova@phystech.edu>
2015-04-20 16:42:41 +0300

    RBTM-155

--- robotom/storage/views.py
+++ robotom/storage/views.py
@@ -1,18 +1,31 @@
 # coding=utf-8
 from django.shortcuts import render
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 
 def storage_view(request):
     # TODO
+
     if request.method == "GET":
         return render(request, 'storage/storage_index.html', {
-            'record_range': xrange(10),
             'caption': 'Хранилище',
             'toShowResult': False,
         })
     elif request.method == "POST":
+        records_list = range(110)
+        pagin = Paginator(records_list, 20)
+        page = request.GET.get('page')
+
+        try:
+            records = pagin.page(page)
+        except PageNotAnInteger:
+            # If page is not an integer, deliver first page.
+            records = pagin.page(1)
+        except EmptyPage:
+            # If page is out of range (e.g. 9999), deliver last page of results.
+            records = pagin.page(pagin.num_pages)
         return render(request, 'storage/storage_index.html', {
-            'record_range': xrange(10),
+            'record_range': records,
             'caption': 'Хранилище',
             'toShowResult': True,
         })
