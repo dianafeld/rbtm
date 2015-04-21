e758f2e27dbc316afb8ce8699c80f8b39a681154
Arlenka <alenarykunova@gmail.com>
2015-04-21 21:56:50 +0300

    RBTM-206 добавлены исключения

--- tango_ds/XRayShutter/driver_shutter.py
+++ tango_ds/XRayShutter/driver_shutter.py
@@ -3,6 +3,7 @@ __author__ = 'arlen'
 import serial
 import sys
 import re
+import PyTango
 
 
 class Shutter:
@@ -14,12 +15,21 @@ class Shutter:
     def open_port(self):
         # инициализировать порт
         # '/dev/ttyACM0'
-        self.port = serial.Serial('/dev/pts/9', timeout=Shutter.TIMEOUT)
+        try:
+            self.port = serial.Serial('/dev/pts/9', timeout=Shutter.TIMEOUT)
+        except serial.SerialException:
+            python_exception = PyTango.Except.to_dev_failed(*sys.exc_info())
+            PyTango.Except.re_throw_exception(python_exception,
+                                              "XRayShutter_ConnectionFailed",
+                                              "Can't open a port",
+                                              "Shutter::open_port()")
 
         # проверить работоспособность модуля
         self.port.write('$KE\r\n')
         if self.port.readline() != '#OK\r\n':
-            sys.stderr.write("Module isn't in workable state\n")
+            PyTango.Except.throw_exception("XRayShutter_ConnectionFailed",
+                                           "Module isn't in workable state",
+                                           "Shutter::open_port()")
 
     def __init__(self):
         self.port = serial.Serial()
@@ -29,7 +39,7 @@ class Shutter:
     def answer_is_correct(self, message, regex):
         pattern = re.compile(regex)
         answer = pattern.match(message)
-        if answer is not None:
+        if answer != None:
             if answer.group() == message:
                 return True
         return False
@@ -44,24 +54,35 @@ class Shutter:
             # взять значение из строчки
             relay_state = relay_state.split(',')[2]
         else:
-            sys.stderr.write("Unexpected answer from module, should be '#RDR,1,<Value>', got '" + relay_state + "'\n")
-
+            PyTango.Except.throw_exception("XRayShutter_UnexpectedValue",
+                                           "Unexpected answer from module, should be '#RDR,1,<Value>', got '" + relay_state + "'",
+                                           "Shutter::is_open()")
         return [False, True][int(relay_state)]
 
     def set_value(self, val):
         # 1 открыть заслону, 0 закрыть заслонку
         if not val in [0, 1]:
-            sys.stderr.write("Trying to set incorrect value to shutter: expected 0 or 1, got " + str(val) + "\n")
+            PyTango.Except.throw_exception("XRayShutter_IllegalArgument",
+                                       "Trying to set incorrect value to shutter: expected 0 or 1, got '" + str(val) + "'",
+                                       "Shutter::set_value()")
 
         # установить значение на реле (1 замкнуть, 0 разомкнуть)
         self.port.write("$KE,REL,1," + str(val) + "\r\n")
 
         # проверить, что значение установилось
         if self.port.readline() != "#REL,OK\r\n":
-            sys.stderr.write("Can't set value " + str(val) + " to relay\n")
+            PyTango.Except.throw_exception("XRayShutter_IOException",
+                                           "Can't set value " + str(val) + " to relay",
+                                           "Shutter::set_value()")
 
     def open(self):
-        self.set_value(1)
+        try:
+            self.set_value(1)
+        except PyTango.DevFailed as df:
+            PyTango.Except.re_throw_exception(df, df[-1].reason, df[-1].desc, "Shutter::open()\n\t" + df[-1].origin)
 
     def close(self):
-        self.set_value(0)
+        try:
+            self.set_value(0)
+        except PyTango.DevFailed as df:
+            PyTango.Except.re_throw_exception(df, df[-1].reason, df[-1].desc, "Shutter::close()\n\t" + df[-1].origin)
