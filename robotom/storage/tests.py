cabd0ee1cf9ae635a5ba32252cf837ff46943a08
Anna.Savinova <anna.savinova@phystech.edu>
2015-04-28 18:14:39 +0300

    RBTM-229 rename tests

--- robotom/storage/tests.py
+++ robotom/storage/tests.py
@@ -3,12 +3,12 @@ from django.test import TestCase
 
 
 class StorageIndexTest(TestCase):
-    def LogInTest(self):
+    def test_login(self):
         response = self.client.get("/storage/")
         self.assertEqual(response.status_code, 201)
 
-    def PaginationTest(self):
+    def test_pagination(self):
         response = self.client.post("/storage/",
                                     {"KeyWords": "KeyWords", "SinceDate": "2015-04-08", "ToDate": "2015-04-16",
-                                     "Finished": "Завершен", "Owner": "Owner"})
+                                     "Finished": "---", "Owner": "Owner"})
         self.assertEqual(response.status_code, 200)
