8462b52f5832edcddd4601abce976e164f38db52
sobolev.1994 <sobolev.1994@mail.ru>
2015-04-29 22:54:25 +0400

    RBTM-240 RBTM-241

--- robotom/experiment/urls.py
+++ robotom/experiment/urls.py
@@ -3,4 +3,9 @@ from experiment import views
 
 urlpatterns = patterns('',
                        url(r'^$', views.experiment_view, name='index'),
-                       )
+                       url(r'^on', views.check, name='check'),
+                       url(r'^start', views.experiment_start, name='experiment_on'),
+                       url(r'^send', views.send_parameters, name='send_parameters'),
+                       url(r'^image', views.show_image, name='show_image'),                       
+                       
+						)
