84f39f7a62a362cca99511035f091675fd94e9a6
Diana Ichalova <diana.ichalova@phystech.edu>
2015-05-06 21:48:31 +0300

    RBTM-288 detector make_image speedup

--- tango_ds/Detector/xiApi.pyx
+++ tango_ds/Detector/xiApi.pyx
@@ -88,6 +88,7 @@ cdef void create_exception(return_code, func_name) except *:
     print(reason, desc, origin)
     PyTango.Except.throw_exception(reason, desc, origin)
 
+
 cdef void handle_error(return_code, func_name) except *:
     if return_code != XI_OK:
         create_exception(return_code, func_name)
@@ -97,7 +98,8 @@ cdef void handle_error(return_code, func_name) except *:
 cdef class Detector:
 
     cdef HANDLE handle
-    TIMEOUT = 10000 # in ms
+    TIMEOUT = 30 * 1000  # in ms
+
     def __cinit__(self):
         cdef DWORD a
 
@@ -111,7 +113,7 @@ cdef class Detector:
         e = xiSetParamInt(self.handle, XI_PRM_IMAGE_DATA_FORMAT, XI_MONO16)
         handle_error(e, "Detector.__cinit__()")
 
-        e = xiSetParamInt(self.handle, XI_PRM_BUFFER_POLICY, XI_BP_SAFE);
+        e = xiSetParamInt(self.handle, XI_PRM_BUFFER_POLICY, XI_BP_SAFE)
         handle_error(e, "Detector.__cinit__()")
 
     def set_exposure(self, exposure):
@@ -122,47 +124,32 @@ cdef class Detector:
         cdef int exposure_in_us
         e = xiGetParamInt(self.handle, XI_PRM_EXPOSURE, &exposure_in_us)
         handle_error(e, "Detector.get_exposure()")
-        return round(exposure_in_us / 1000)
+        return round(float(exposure_in_us) / 1000)
 
     cdef make_image(self, XI_IMG image):
-        img = [[] for i in range(image.height)]
-        i = 0
-        for h in range(image.height):
-            for w in range(image.width + image.padding_x / 2):
-                if (<numpy.uint16_t *>image.bp)[i] <= 10:
-                    print(i, (<numpy.uint16_t *>image.bp)[i])
-                img[h].append((<numpy.uint16_t *>image.bp)[i])
-                i += 1
-
-        return img
-
-    cdef make_image2(self, XI_IMG image):
         number_of_pixels = (image.width + image.padding_x / 2) * image.height
         size = number_of_pixels * 2
-        img = numpy.empty(shape=(image.height, image.width+ image.padding_x / 2), dtype = 'uint16')
-        i = 0
-        for h in range(img.shape[0]):
-            for w in range(img.shape[1]):
-                img[h,w]= (<numpy.uint16_t *>image.bp)[i]
-                i = i+1    
+        cdef numpy.ndarray[numpy.uint16_t, ndim=2] img = numpy.empty(
+            shape=(image.height, image.width+ image.padding_x / 2),
+            dtype = 'uint16')
+        memcpy(<void *> img.data, image.bp, size)
         return img
 
     def get_image(self):
-
-
         cdef XI_IMG image
+
         e = xiStartAcquisition(self.handle)
         handle_error(e, "Detector.get_image().xiStartAcquisition()")
+
         image.bp = NULL
         image.bp_size = 0
-        for i in range(2):
-            e = xiGetImage(self.handle, Detector.TIMEOUT, &image)
-            handle_error(e, "Detector.get_image().xiGetImage()")
+        e = xiGetImage(self.handle, Detector.TIMEOUT, &image)
+        handle_error(e, "Detector.get_image().xiGetImage()")
 
         e = xiStopAcquisition(self.handle)
         handle_error(e, "Detector.get_image().xiStopAcquisition()")
-        
-        return self.make_image2(image)    
+
+        return self.make_image(image)
 
     def enable_cooling(self):
         e = xiSetParamInt(self.handle, XI_PRM_COOLING, XI_ON)
