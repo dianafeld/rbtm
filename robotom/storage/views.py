37cb2d9043c7feabc6995600402516526914f070
anna.savinova <anna.savinova@phystech.edu>
2015-05-18 22:26:11 +0300

    RBTM-264

--- robotom/storage/views.py
+++ robotom/storage/views.py
@@ -269,7 +269,6 @@ def storage_record_view(request, storage_record_id):
     return render(request, 'storage/storage_record_new.html', {
         "record_id": storage_record_id,
         'caption': 'Запись хранилища номер ' + str(storage_record_id),
-        'image_range': xrange(1, 5),  # TODO
         'to_show': to_show,
         'info': record,
         'frames_list': frames_list,
