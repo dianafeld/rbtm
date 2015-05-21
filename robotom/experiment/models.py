5111987ad6ee0dceca17f4c70fc02b73b8e963b2
do.kseniya <do.kseniya@gmail.com>
2015-05-21 05:27:39 -0500

    RBTM-279 RBTM-280

--- robotom/experiment/models.py
+++ robotom/experiment/models.py
@@ -3,10 +3,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Tomo(models.Model):
-	condition = models.CharField(max_length = 1)
+class Tomograph(models.Model):
+	state = models.CharField(max_length = 15)
 
-class Experiment(models.Model):
-    exp_id = models.CharField(max_length = 100) 
-    user = models.OneToOneField(User)
     
