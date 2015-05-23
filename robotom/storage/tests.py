c034bd7f5fe38a0653cf3c61c28b0ce4c032e2e5
anna.savinova <anna.savinova@phystech.edu>
2015-05-23 17:27:07 +0300

    RBTM-264 tests

--- robotom/storage/tests.py
+++ robotom/storage/tests.py
@@ -8,7 +8,7 @@ class StorageIndexTest(TestCase):
         response = self.client.get("/storage/")
         self.assertEqual(response.status_code, 200)
 
-    def test_search_response(self):
+    def test_search_empty(self):
         response = self.client.post('/storage/',
                                     {'Specimen': '', 'DarkFromCount': '', 'DarkToCount': '', 'DarkFromExposure': '',
                                      'DarkToExposure': '', 'EmptyFromCount': '', 'EmptyToCount': '',
@@ -18,6 +18,32 @@ class StorageIndexTest(TestCase):
                                      'DataToCountPerStep': '', 'DataFromStepCount': '', 'DataToStepCount': ''})
         self.assertEqual(response.status_code, 200)
 
-    def test_record_page(self):
-        response = self.client.get('/storage/')
+    def test_search_specimen(self):
+        response = self.client.post('/storage/',
+                                    {'Specimen': 'unreal object!!!___@#', 'DarkFromCount': '', 'DarkToCount': '',
+                                     'DarkFromExposure': '',
+                                     'DarkToExposure': '', 'EmptyFromCount': '', 'EmptyToCount': '',
+                                     'EmptyFromExposure': '', 'EmptyToExposure': '', 'Finished': '',
+                                     'Advanced': '', 'DataFromExposure': '', 'DataToExposure': '',
+                                     'DataFromAngleStep': '', 'DataToAngleStep': '', 'DataFromCountPerStep': '',
+                                     'DataToCountPerStep': '', 'DataFromStepCount': '', 'DataToStepCount': ''})
         self.assertEqual(response.status_code, 200)
+        self.assertTrue("alert alert-danger" in response.content)
+
+    def test_record_page_prohibit_symbols(self):
+        response = self.client.get('/storage/storage_record_unreal_record/')
+        self.assertEqual(response.status_code, 404)
+        response = self.client.get('/storage/storage_record_unreal*record/')
+        self.assertEqual(response.status_code, 404)
+        response = self.client.get('/storage/storage_record_unreal(record/')
+        self.assertEqual(response.status_code, 404)
+        response = self.client.get('/storage/storage_record_unreal)record/')
+        self.assertEqual(response.status_code, 404)
+        response = self.client.get('/storage/storage_record_unreal@record/')
+        self.assertEqual(response.status_code, 404)
+
+    def test_record_page(self):
+        response = self.client.get('/storage/storage_record_unreal-record/')
+        self.assertEqual(response.status_code, 200)
+
+        self.assertTrue("alert alert-danger" in response.content)
