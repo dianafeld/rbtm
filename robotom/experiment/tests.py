c952d9a23e0d0cca7652692e295afbd2cfa293fb
sobolev.1994 <sobolev.1994@mail.ru>
2015-05-24 05:46:29 +0400

    RBTM-274 RBTM-275

--- robotom/experiment/tests.py
+++ robotom/experiment/tests.py
@@ -8,10 +8,41 @@ class ExpPageTest(TestCase):
     c = Client()
     response = c.get('/experiment/')
     self.assertEquals(response.status_code, 200)
-    
+
+    def test_search_empty(self):
+        response = self.client.post('/experiment/interface',
+									{
+										'experiment id': '0536ba11-548a-4e98-92a7-61f126235332',
+										'specimen': 'test',
+										'tags': ,
+										'experiment parameters':
+											{
+												'advanced': False,
+												'DARK':
+												    {
+												        'count': 1,
+												        'exposure': 1000,0
+												    },
+												'EMPTY':
+												    {
+												        'count': 1,
+												        'exposure': 1000,0
+												    },
+												'DATA':
+												    {
+												        'step count': 1,
+												        'exposure': 1000,0,
+												        'angle step': 10,0,
+												        'count per step': 1
+												    }
+											}
+									})
+        self.assertEqual(response.status_code, 200)
+   
 if __name__ == '__main__':
     unittest.main(
         testRunner=xmlrunner.XMLTestRunner(output='test-reports'),
         failfast=False, buffer=False, catchbreak=False
     )
+
 
