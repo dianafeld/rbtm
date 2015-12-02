55ecbc0d4e0a9cb64c58200f60876ff714a2629e
Rustam Mardanov <rustam.zftsh@gmail.com>
2015-12-02 19:14:13 +0300

    now write files for several levels at a time

--- storage/visualization/update_points.py
+++ storage/visualization/update_points.py
@@ -1,141 +1,133 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
-
 import sys
 import h5py
 import numpy as np
 import matplotlib as mpl
 import matplotlib.cm as cm
-
+import os
 
 def main():
 
-
     RAREFACTION = 1
-    LEVEL = 5
+    LEVEL1, LEVEL2 = 10, 10
+    # Script will create files for each level between LEVEL1 and LEVEL2 including
     COLORMAP = cm.hsv
 
+
+    # User can input RAREFACTION, LEVEL1 and LEVEL2
     if len (sys.argv) > 1:
-        LEVEL = int(sys.argv[1])
+        RAREFACTION = int(sys.argv[1])
         if len (sys.argv) > 2:
-            RAREFACTION = int(sys.argv[2])
-    print "LEVEL = ", LEVEL, "  RAREFACTION = ",  RAREFACTION
+            LEVEL1 = int(sys.argv[2])
+            if len (sys.argv) > 3:
+                LEVEL2 = int(sys.argv[3])
+            else:
+                LEVEL2 = LEVEL1
+    
+    # Correcting values inputed by user
+    RAREFACTION = max(RAREFACTION, 1)
+    LEVEL1 = min (max (LEVEL1, 0), 25)
+    LEVEL2 = min (max (LEVEL2, 0), 25)
+    if LEVEL2 < LEVEL1:
+        LEVEL1, LEVEL2 = LEVEL2, LEVEL1
 
-    percent_to_left = (0.5 ** LEVEL) * 100
-
-    print ("%.4f%% points with biggest values will left" % percent_to_left)
-
+    levels = range(LEVEL1, LEVEL2+1)
+    print "RAREFACTION = ",  RAREFACTION, "  LEVELS = ", levels
 
 
     hfd5FileName = "largeData/hand/result.hdf5"
-    outputFileName = "largeData/hand/LVL" + str(LEVEL) + "_RF" + str(RAREFACTION) + ".js"
-    # !! ATTENTION, THIS FILE WILL BE TOTALLY OWERWRITTEN !!
-
+    outputFileNamePrefix = "largeData/hand/RF" + str(RAREFACTION)
 
 
     hdf5File = h5py.File(hfd5FileName, 'r')
-    
     dataCube = hdf5File["Results"]
     print "Original cube shape:", dataCube.shape
 
     print("Rarefying dataCube...")
     if (RAREFACTION > 1):
-        rarefiedDataCube = dataCube[::RAREFACTION, ::RAREFACTION, ::RAREFACTION]
+        dataCube = dataCube[::RAREFACTION, ::RAREFACTION, ::RAREFACTION]
     else:
-        rarefiedDataCube = dataCube[::2,:,:]
+        dataCube = dataCube[::2,:,:]
 
-    print "Rarefied cube shape:", rarefiedDataCube.shape
-    N, M, K = rarefiedDataCube.shape
+    print "Rarefied cube shape:", dataCube.shape
+    N, M, K = dataCube.shape
 
-    minValue = np.min(rarefiedDataCube)
-    maxValue = np.max(rarefiedDataCube)
+    minValue = np.min(dataCube)
+    maxValue = np.max(dataCube)
     if maxValue == minValue:
         print "All values are the same - look for better data!\nStop."
         return
 
-    print("Looking for threshold...")
-    threshold = np.percentile(rarefiedDataCube, (100 - percent_to_left))
-
-    print("Throwing away points with small values...")
-    Ix, Iy, Iz = np.where(rarefiedDataCube > threshold)
-    numVertices =  len(Ix)
-    A = np.empty((numVertices), dtype=float)
-    for i in xrange(numVertices):
-        A[i] = rarefiedDataCube[ Ix[i], Iy[i], Iz[i] ]
+    print("Looking for thresholds...")
+    list_of_percents_to_left = [  (1 - 0.5 ** level) * 100   for level in levels ]
+    thresholds_list = np.percentile(dataCube, list_of_percents_to_left)
+    thresholds_dict = dict( zip(levels, thresholds_list) )
+    print("Done")
 
 
-    print("Calculating color values...")
-    # this block normalizes all values
-    A = (A - minValue) / (maxValue - minValue)
-    threshold
+    for level in levels:
+        print ("Creating file with level: %d" % level)
+        outputFileName =  outputFileNamePrefix + "_LVL" + str(level) +  ".js"
 
-    norm = mpl.colors.Normalize(vmin=(threshold - minValue) /  (maxValue - minValue), vmax=1)
-    m = cm.ScalarMappable(norm=norm, cmap=COLORMAP)
+        threshold = thresholds_dict[level]
+        print("    Throwing away points with small values...")
+        Ix, Iy, Iz = np.where(dataCube > threshold)
 
-    RGBA = m.to_rgba(A)
-    RGBA = RGBA * 512
-    RGBA = RGBA.astype(int)
-
-
-    A = A * 512
-    A = A.astype(int)
-
-    R = (RGBA[:,0]).flatten()
-    G = (RGBA[:,1]).flatten()
-    B = (RGBA[:,2]).flatten()
-
-    if RAREFACTION != 1:
-        X = Ix - N / 2
-    else:
-        X = 2 * Ix - N
-        # strange multiplying by 2 when RAREFACTION==1, it's becuase of using half of 
-        # array, not full (look at code above where array is rarefied)
-    Y = Iy - M / 2
-    Z = Iz - K / 2
+        print("    Calculating values to write...")
+        numVertices =  len(Ix)
+        left_values = np.empty((numVertices), dtype=float)
+        for i in xrange(numVertices):
+            left_values[i] = dataCube[ Ix[i], Iy[i], Iz[i] ]
 
 
-    f = open(outputFileName, 'w')
-    print("Writing to file...")
-    f.seek(0)
-    f.truncate()
-    f.write("var NMK = [%d, %d, %d]" % (N, M, K))
 
-    print("Writing R values...")
-    f.write(";\nvar R_arr = ")
-    f.write(str(R.tolist()))
+        norm = mpl.colors.Normalize(vmin=threshold, vmax=maxValue)
+        m = cm.ScalarMappable(norm=norm, cmap=COLORMAP)
+        RGBA = m.to_rgba(left_values)
+        RGBA = (RGBA * 512).astype(int)
 
-    print("Writing G values...")
-    f.write(";\nvar G_arr = ")
-    f.write(str(G.tolist()))
+        R, G, B = RGBA[:, 0], RGBA[:, 1], RGBA[:, 2]
 
-    print("Writing B values...")
-    f.write(";\nvar B_arr = ")
-    f.write(str(B.tolist()))
+        A = left_values
+        del(left_values)
+        A = (A - minValue) / (maxValue - minValue)
+        A = (A * 512).astype(int)
 
-    print("Writing A values...")
-    f.write(";\nvar A_arr = ")
-    f.write(str(A.tolist()))
-
-    print("Writing X values...")
-    f.write(";\nvar X_arr = ")
-    f.write(str(X.tolist()))
-
-    print("Writing Y values...")
-    f.write(";\nvar Y_arr = ")
-    f.write(str(Y.tolist()))
-
-    print("Writing Z values...")
-    f.write(";\nvar Z_arr = ")
-    f.write(str(Z.tolist()))
+        if RAREFACTION != 1:
+            X = Ix - N / 2
+        else:
+            X = 2 * Ix - N
+            # strange multiplying by 2 when rarefaction==1, it's becuase of using half of 
+            # array, not full (look at code above where array is rarefied)
+        Y = Iy - M / 2
+        Z = Iz - K / 2
 
 
-    f.write(";\n")
-    f.write("var numVertices = " + str(numVertices) + ";\n")
+        f = open(outputFileName, 'w')
+        print("    Writing to file...")
+        f.seek(0)
+        f.truncate()
+        f.write("var NMK = [%d, %d, %d]" % (N, M, K))
 
-    f.close()
+        f.write(";\nvar R_arr = " + str(R.tolist()))
+        f.write(";\nvar G_arr = " + str(G.tolist()))
+        f.write(";\nvar B_arr = " + str(B.tolist()))
+        f.write(";\nvar A_arr = " + str(A.tolist()))
+        f.write(";\nvar X_arr = " + str(X.tolist()))
+        f.write(";\nvar Y_arr = " + str(Y.tolist()))
+        f.write(";\nvar Z_arr = " + str(Z.tolist()))
+
+        f.write(";\nvar numVertices = " + str(numVertices) + ";\n")
+        f.close()
+
+        print("    Finish writing, filename is: '%s', size of file: %.1f kB" 
+              % (outputFileName, float(os.stat(outputFileName).st_size)/1024.0) )
+        print("    Number of leftover vertices: %d,  %.2f%% from all" % (numVertices, float(numVertices * 100)/float(N * M * K)))
+
     hdf5File.close()
-    print("Finish writing, file is closed")
-    print("Number of leftover vertices: %d,  %.2f%% from all" % (numVertices, float(numVertices * 100)/float(N * M * K)))
+    print("Finish")
+
 
 if __name__ == "__main__":
     main()
