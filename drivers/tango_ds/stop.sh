8135ea9f16304f0d735c0e8c15cf8792592f883f
Alexey Buzmakov <buzmakov@gmail.com>
2015-12-04 15:49:14 +0300

    fixing stop.sh for killing only tango servers and docker-compose drivers

--- drivers/tango_ds/stop.sh
+++ drivers/tango_ds/stop.sh
@@ -2,9 +2,9 @@
 
 # killall -v python
 
+pkill -e -f Tomograph/Tomograph.py
 pkill -e -f AngleMotor/AngleMotor.py
 pkill -e -f HorizontalMotor/HorizontalMotor.py
 pkill -e -f Detector/Detector.py
 pkill -e -f XRayShutter/XRayShutter.py
 pkill -e -f XRaySource/XRaySource.py
-pkill -e -f Tomograph/Tomograph.py
